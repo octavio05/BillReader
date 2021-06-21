d0a28866e870d181d218edb49a24668d4aa2c2be
octavio05 <osq.005@gmail.com>
2021-06-21 19:05:08 +0100

    Se crea la clase PdfInfo

---
+++ BillReader/Entities/PdfInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BillReader.Entities
+{
+    /// <summary>
+    ///     Contiene información del documento pdf.
+    /// </summary>
+    public class PdfInfo
+    {
+
+        /// <summary>
+        ///     Texto de las páginas del documento pdf.
+        /// </summary>
+        public IEnumerable<string> Pages { get; set; }
+        /// <summary>
+        ///     Nombre del documento.
+        /// </summary>
+        public string FileName { get; set; }
+
+    }
+}
